f630464b5add6ee91cd9de0c8f915d8aa1269df6
Tayeb Chlyah <tayebchlyah@gmail.com>
2022-02-25 20:15:43 +0100

    Add e2e profile constant

--- jhipster-framework/src/main/java/tech/jhipster/config/JHipsterConstants.java
+++ jhipster-framework/src/main/java/tech/jhipster/config/JHipsterConstants.java
@@ -29,6 +29,8 @@ public interface JHipsterConstants {
     String SPRING_PROFILE_DEVELOPMENT = "dev";
     /** Constant <code>SPRING_PROFILE_TEST="test"</code> */
     String SPRING_PROFILE_TEST = "test";
+    /** Constant <code>SPRING_PROFILE_E2E="e2e"</code> */
+    String SPRING_PROFILE_E2E = "e2e";
     /** Constant <code>SPRING_PROFILE_PRODUCTION="prod"</code> */
     String SPRING_PROFILE_PRODUCTION = "prod";
     /** Spring profile used when deploying with Spring Cloud (used when deploying to CloudFoundry)
