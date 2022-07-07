ef7906e9165413d1b035c38344121dff17a267ac
Frederik Hahne <atomfrede@gmail.com>
2022-07-07 14:52:49 +0200

    correctly use seconds for caching header
    closes https://github.com/jhipster/generator-jhipster/issues/19102

--- jhipster-framework/src/test/java/tech/jhipster/web/filter/CachingHttpHeadersFilterTest.java
+++ jhipster-framework/src/test/java/tech/jhipster/web/filter/CachingHttpHeadersFilterTest.java
@@ -61,8 +61,7 @@ class CachingHttpHeadersFilterTest {
 
     @Test
     void testWithoutInit() {
-        int daysToLive = CachingHttpHeadersFilter.DEFAULT_DAYS_TO_LIVE;
-        long secsToLive = TimeUnit.DAYS.toMillis(daysToLive);
+        long secsToLive = CachingHttpHeadersFilter.DEFAULT_SECONDS_TO_LIVE;
 
         long before = System.currentTimeMillis();
         before -= before % 1000L;
@@ -85,7 +84,7 @@ class CachingHttpHeadersFilterTest {
     @Test
     void testWithInit() {
         int daysToLive = CachingHttpHeadersFilter.DEFAULT_DAYS_TO_LIVE >>> 2;
-        long secsToLive = TimeUnit.DAYS.toMillis(daysToLive);
+        long secsToLive = TimeUnit.DAYS.toSeconds(daysToLive);
         properties.getHttp().getCache().setTimeToLiveInDays(daysToLive);
 
         long before = System.currentTimeMillis();
