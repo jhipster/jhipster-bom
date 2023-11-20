ddbe91240e079a749da9ce185951cb7468c3c454
DISHA ANAND <ds442718@dal.ca>
2023-11-20 18:06:53 -0400

    sonar fix: https://sonarcloud.io/project/issues?fileUuids=AYQzN3Lt2_PoappU_KSf&resolved=false&types=CODE_SMELL&id=jhipster-framework&open=AYQzN3fW2_PoappU_KSh
    sonar fix: https://sonarcloud.io/project/issues?fileUuids=AYQzN3Lt2_PoappU_KSf&resolved=false&types=CODE_SMELL&id=jhipster-framework&open=AYQzN3fW2_PoappU_KSj
    
    sonar fix: https://sonarcloud.io/project/issues?fileUuids=AYQzN3Lt2_PoappU_KSf&resolved=false&types=CODE_SMELL&id=jhipster-framework&open=AYQzN3fW2_PoappU_KSk

--- jhipster-framework/src/main/java/tech/jhipster/service/ConditionBuilder.java
+++ jhipster-framework/src/main/java/tech/jhipster/service/ConditionBuilder.java
@@ -1,22 +1,3 @@
-/*
- * Copyright 2016-2023 the original author or authors from the JHipster project.
- *
- * This file is part of the JHipster project, see https://www.jhipster.tech/
- * for more information.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package tech.jhipster.service;
 
 import java.time.LocalDateTime;
@@ -24,12 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-
 import org.springframework.data.relational.core.sql.Column;
 import org.springframework.data.relational.core.sql.Condition;
 import org.springframework.data.relational.core.sql.Conditions;
 import org.springframework.data.relational.core.sql.SQL;
-
 import tech.jhipster.service.filter.BooleanFilter;
 import tech.jhipster.service.filter.DurationFilter;
 import tech.jhipster.service.filter.Filter;
@@ -45,14 +24,14 @@ import tech.jhipster.service.filter.ZonedDateTimeFilter;
  */
 public class ConditionBuilder {
 
-    private final List<Condition> allFilters = new ArrayList<Condition>();
+    private final List<Condition> allFilters = new ArrayList<>();
 
     private final ColumnConverterReactive columnConverter;
 
     public ConditionBuilder(ColumnConverterReactive columnConverter) {
         this.columnConverter = columnConverter;
     }
-    
+
     /**
      * Method that takes in a filter field and a column to construct a compounded SQL Condition.
      * The built condition can be retrieved using the buildConditions function
@@ -84,7 +63,7 @@ public class ConditionBuilder {
     }
 
     /**
-     * Method that builds and returns the compounded Condition object. This method can be called 
+     * Method that builds and returns the compounded Condition object. This method can be called
      * multiple times as the Conditions are being built.
      * @return returns the compounded Condition object
      */
@@ -98,42 +77,30 @@ public class ConditionBuilder {
                 }
             );
     }
-    
+
     private <X> Function<X, String> columnValueConverter(Class<?> targetClass) {
         if (targetClass != null) {
-            return (value) -> columnConverter.convert(value, targetClass).toString();
+            return value -> columnConverter.convert(value, targetClass).toString();
         } else {
-            return (value) -> value.toString();
+            return value -> value.toString();
         }
     }
 
     private <X extends Comparable<? super X>> void buildRangeConditions(RangeFilter<X> rangeData, Column column, Class<?> targetClass) {
         var converterFunction = columnValueConverter(targetClass);
         if (rangeData.getGreaterThan() != null) {
-            allFilters.add(
-                Conditions.isGreater(column, SQL.literalOf(converterFunction.apply(rangeData.getGreaterThan())))
-            );
+            allFilters.add(Conditions.isGreater(column, SQL.literalOf(converterFunction.apply(rangeData.getGreaterThan()))));
         }
         if (rangeData.getLessThan() != null) {
-            allFilters.add(
-                Conditions.isLess(column, SQL.literalOf(converterFunction.apply(rangeData.getLessThan())))
-            );
+            allFilters.add(Conditions.isLess(column, SQL.literalOf(converterFunction.apply(rangeData.getLessThan()))));
         }
         if (rangeData.getGreaterThanOrEqual() != null) {
             allFilters.add(
-                Conditions.isGreaterOrEqualTo(
-                    column,
-                    SQL.literalOf(converterFunction.apply(rangeData.getGreaterThanOrEqual()))
-                )
+                Conditions.isGreaterOrEqualTo(column, SQL.literalOf(converterFunction.apply(rangeData.getGreaterThanOrEqual())))
             );
         }
         if (rangeData.getLessThanOrEqual() != null) {
-            allFilters.add(
-                Conditions.isLessOrEqualTo(
-                    column,
-                    SQL.literalOf(converterFunction.apply(rangeData.getLessThanOrEqual()))
-                )
-            );
+            allFilters.add(Conditions.isLessOrEqualTo(column, SQL.literalOf(converterFunction.apply(rangeData.getLessThanOrEqual()))));
         }
     }
 
@@ -190,24 +157,16 @@ public class ConditionBuilder {
     private <X> void buildGeneralConditions(Filter<X> generalData, Column column, Class<?> targetClass) {
         var converterFunction = columnValueConverter(targetClass);
         if (generalData.getEquals() != null) {
-            allFilters.add(
-                Conditions.isEqual(column, SQL.literalOf(converterFunction.apply(generalData.getEquals())))
-            );
+            allFilters.add(Conditions.isEqual(column, SQL.literalOf(converterFunction.apply(generalData.getEquals()))));
         }
         if (generalData.getNotEquals() != null) {
-            allFilters.add(
-                Conditions.isNotEqual(column, SQL.literalOf(converterFunction.apply(generalData.getNotEquals())))
-            );
+            allFilters.add(Conditions.isNotEqual(column, SQL.literalOf(converterFunction.apply(generalData.getNotEquals()))));
         }
         if (generalData.getIn() != null && generalData.getIn().size() > 0) {
             allFilters.add(
                 Conditions.in(
                     column,
-                    generalData
-                        .getIn()
-                        .stream()
-                        .map(eachIn -> SQL.literalOf(converterFunction.apply(eachIn)))
-                        .collect(Collectors.toList())
+                    generalData.getIn().stream().map(eachIn -> SQL.literalOf(converterFunction.apply(eachIn))).collect(Collectors.toList())
                 )
             );
         }
