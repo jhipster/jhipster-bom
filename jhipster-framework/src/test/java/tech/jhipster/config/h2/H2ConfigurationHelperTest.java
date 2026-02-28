b4f1fdec20340e3ce6ffe189e2a03c6ac8d1d462
Quentin <quentin.monmert@gmail.com>
2026-02-28 18:04:31 +0100

    Fix: URL(java.lang.String) in java.net.URL has been deprecated

--- jhipster-framework/src/test/java/tech/jhipster/config/h2/H2ConfigurationHelperTest.java
+++ jhipster-framework/src/test/java/tech/jhipster/config/h2/H2ConfigurationHelperTest.java
@@ -2,7 +2,7 @@ package tech.jhipster.config.h2;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
-import java.net.URL;
+import java.net.URI;
 import org.junit.jupiter.api.Test;
 
 class H2ConfigurationHelperTest {
@@ -11,7 +11,7 @@ class H2ConfigurationHelperTest {
     void initH2Console() {
         assertThatCode(() -> {
             H2ConfigurationHelper.initH2Console("src/test/resources");
-            new URL("http://localhost:8092").openConnection().connect();
+            URI.create("http://localhost:8092").toURL().openConnection().connect();
         }).doesNotThrowAnyException();
     }
 }
